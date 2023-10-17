82bbe3ec9b87dce9aa9b76097668d075f26a5e67
SpaikSaucus <sernapoli@gmail.com>
2023-10-17 14:48:52 -0300

    GRAL: Comment info request and response process by LoggingBehavior.

--- src/AuthorizationOperation.Application/Behaviors/LoggingBehavior.cs
+++ src/AuthorizationOperation.Application/Behaviors/LoggingBehavior.cs
@@ -15,11 +15,15 @@ namespace AuthorizationOperation.Application.Behaviors
         }
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            this.logger.LogInformation("Executing command {CommandName} with request {@Command}", request.GetType().FullName, request);
+            // uncomment for more details.
+            //this.logger.LogInformation("Executing command {CommandName} with request {@Command}", request.GetType().FullName, request);
+            this.logger.LogInformation("Executing command {CommandName}", request.GetType().FullName);
 
             TResponse response = await next();
 
-            this.logger.LogInformation("Executed command {CommandName} with response {@Response}", request.GetType().FullName, response);
+            // uncomment for more details.
+            //this.logger.LogInformation("Executed command {CommandName} with response {@Response}", request.GetType().FullName, response);
+            this.logger.LogInformation("Executed command {CommandName}", request.GetType().FullName);
 
             return response;
         }
