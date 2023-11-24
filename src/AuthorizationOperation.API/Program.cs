edbd484c79e6ed8bb41bad2624d3488b1c5fbe0e
SpaikSaucus <sernapoli@gmail.com>
2023-11-23 22:49:27 -0300

    GRAL: Inherit appsettings.

--- src/AuthorizationOperation.API/Program.cs
+++ src/AuthorizationOperation.API/Program.cs
@@ -1,7 +1,9 @@
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System;
 
 namespace AuthorizationOperation.API
 {
@@ -15,19 +17,28 @@ namespace AuthorizationOperation.API
         public static IHostBuilder CreateWebHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                        .UseSerilog((hostingContext, loggerConfiguration) =>
-                        {
-                            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
-                        })
-                       .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                       .ConfigureWebHostDefaults(webBuilder =>
-                       {
-                           webBuilder.ConfigureKestrel(serverOptions =>
-                           {
-                               serverOptions.AddServerHeader = false;
-                           })
-                       .UseStartup<Startup>();
-                       });
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+
+                    Console.WriteLine("Env:" + env.EnvironmentName);
+                })
+                .UseSerilog((hostingContext, loggerConfiguration) =>
+                {
+                    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+                })
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.AddServerHeader = false;
+                    })
+                .UseStartup<Startup>();
+                });
         }
     }
 }
